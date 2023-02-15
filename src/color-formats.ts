a1c2b398fe3c74e28e30f565a05ebe80ad0d98d7
moneko <poi.nyaa@qq.com>
2023-02-15 17:34:24 +0800

    css名称替换

--- src/color-formats.ts
+++ src/color-formats.ts
@@ -145,8 +145,8 @@ export const getColorPalette = (
     [`--${opt.name}-color`]: colors[5],
     [`--${opt.name}-color-active`]: colors[6],
     [`--${opt.name}-color-hover`]: colors[4],
-    [`--${opt.name}-color-deprecated-bg`]: colors[0],
-    [`--${opt.name}-color-deprecated-border`]: colors[2],
+    [`--${opt.name}-color-bg`]: colors[0],
+    [`--${opt.name}-color-border`]: colors[2],
     [`--${opt.name}-color-outline`]: tinycolor(colors[5]).setAlpha(0.2).toRgbString(),
   };
 };
