a3fd706e4536bb5d1ff52fb27e8c4624b267c411
Moneko <poi.nyaa@qq.com>
2025-04-02 11:39:20 +0800

    Update setClipboard.ts

--- src/setClipboard.ts
+++ src/setClipboard.ts
@@ -1,5 +1,61 @@
 import isFunction from './isFunction';
 
+function clipboardTimer(t: HTMLElement | Element) {
+  let c: number | null = window.setTimeout(() => {
+    t.setAttribute('data-copy-exit', '');
+    if (c !== null) {
+      window.clearTimeout(c);
+      c = null;
+    }
+    let e: number | null = window.setTimeout(() => {
+      t.removeAttribute('data-copy-exit');
+      t.removeAttribute('data-copy');
+      t.removeAttribute('role');
+      t.removeAttribute('aria-live');
+      if (e !== null) {
+        window.clearTimeout(e);
+        e = null;
+      }
+    }, 300);
+  }, 4000);
+}
+
+function success(target?: Element) {
+  if (target) {
+    target.setAttribute('data-copy', 'success');
+    target.setAttribute('role', 'alert');
+    target.setAttribute('aria-live', 'assertive');
+    clipboardTimer(target);
+  }
+}
+function error(target?: Element, onError?: (e: string) => void, e?: string) {
+  if (target) {
+    target.setAttribute('data-copy', 'failure');
+    clipboardTimer(target);
+  }
+  if (isFunction(onError)) {
+    onError(e);
+  }
+}
+function unsecuredClipboard(text: string, target?: Element, onError?: (e: string) => void) {
+  const inp = document.createElement('input');
+
+  inp.value = text;
+  inp.style.position = 'fixed';
+  inp.style.opacity = '0';
+  inp.style.zIndex = '-1000';
+  document.body.appendChild(inp);
+  inp.focus();
+  inp.select();
+  try {
+    document.execCommand('copy');
+    success(target);
+  } catch (e) {
+    error(target, onError, e as string);
+  }
+  document.body.removeChild(inp);
+  inp.remove();
+}
 /**
  * 复制文本到剪切板
  * @param {string} text 内容
@@ -7,61 +63,18 @@ import isFunction from './isFunction';
  * @param {Function} onError 失败的回调
  * @returns {Promise<void>} Promise<void>
  */
-function setClipboard(
-  text: string,
-  target?: Element,
-  // eslint-disable-next-line no-unused-vars
-  onError?: (e: string) => void,
-): void {
-  const clipboardTimer = (t: HTMLElement | Element) => {
-    let c: number | null = window.setTimeout(() => {
-      t.setAttribute('data-copy-exit', '');
-      if (c !== null) {
-        window.clearTimeout(c);
-        c = null;
-      }
-      let e: number | null = window.setTimeout(() => {
-        t.removeAttribute('data-copy-exit');
-        t.removeAttribute('data-copy');
-        t.removeAttribute('role');
-        t.removeAttribute('aria-live');
-        if (e !== null) {
-          window.clearTimeout(e);
-          e = null;
-        }
-      }, 300);
-    }, 4000);
-  };
-
+function setClipboard(text: string, target?: Element, onError?: (e: string) => void): void {
   if (typeof navigator.clipboard === 'undefined') {
-    if (target) {
-      target.setAttribute('data-copy', 'failure');
-      clipboardTimer(target);
-    }
-    if (isFunction(onError)) {
-      onError('Available only in secure contexts.');
-    }
-    return;
-  }
-
-  if (target) {
+    unsecuredClipboard(text, target, onError);
+  } else {
     navigator.clipboard.writeText(text).then(
       function () {
-        target.setAttribute('data-copy', 'success');
-        target.setAttribute('role', 'alert');
-        target.setAttribute('aria-live', 'assertive');
-        clipboardTimer(target);
+        success(target);
       },
       function (e) {
-        target.setAttribute('data-copy', 'failure');
-        clipboardTimer(target);
-        if (isFunction(onError)) {
-          onError(e);
-        }
+        error(target, onError, e as string);
       },
     );
-  } else {
-    navigator.clipboard.writeText(text);
   }
 }
 
